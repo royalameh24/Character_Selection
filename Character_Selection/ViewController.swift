4d8d244f042c1a500e8e62e9810fc9fb5140c920
Roy Alameh <roy.alameh24@d155.org>
2022-08-28 18:00:41 -0500

    Initial Commit

---
+++ Character_Selection/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Character_Selection
+//
+//  Created by ROY ALAMEH on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var outlet1: UIImageView!
+    
+    @IBOutlet weak var outlet2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func supermanAction(_ sender: UIButton) {
+        outlet1.image = UIImage(named: "superman")
+    }
+    
+    @IBAction func batmanAction(_ sender: UIButton) {
+        outlet1.image = UIImage(named: "batman")
+    }
+    
+    @IBAction func flashAction(_ sender: UIButton) {
+        outlet1.image = UIImage(named: "flash")
+    }
+    
+    
+    @IBAction func batAction(_ sender: UIButton) {
+        outlet2.image = UIImage(named: "baseballBat")
+    }
+    
+    @IBAction func katanaAction(_ sender: UIButton) {
+        outlet2.image = UIImage(named: "katana")
+    }
+    
+    @IBAction func greatswordAction(_ sender: UIButton) {
+        outlet2.image = UIImage(named: "greatsword")
+    }
+    
+    
+}
+
